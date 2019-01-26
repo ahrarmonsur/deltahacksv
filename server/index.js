4c3278283d106852999f6d2640157c093fb1b545
Caleb Mech <cmech1@gmail.com>
2019-01-26 15:37:48 -0500

    Created route outlines, connected to db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,75 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+
+const DB_URL = "mongodb://admin:dh5acdg@ds145868.mlab.com:45868/deltahacksv"
+const DB_NAME = "deltahacksv"
+const client = new MongoClient(DB_URL)
+let db
+
+client.connect((err) => {
+    if (err) console.log(err)
+    console.log("Connected successfully to database")
+
+    db = client.db(DB_NAME)
+})
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send("Hello world");
+    res.send("Hello world")
+})
+
+// Patients
+
+app.get('/api/patients/:patient_id', (req, res) => {
+    let patient_id = req.params.patient_id
+})
+
+app.post('/api/patients', (req, res) => {
+    // Create patients
+    db.collections('patients').insertOne({
+        first_name: "David",
+        last_name: "Smith",
+        dob: new Date("1999/03/12"),
+        email: "email@test.com"
+    }, result => res.send(result))
+})
+
+app.get('/api/form/:form_id', (req, res) => {
+    // Get form data
+})
+
+app.post('/api/patients/:patient_id/form', (req, res) => {
+    // Create/Update patients form
+    let collection = db.collections('patients')
+
+    collection.update({$set
+        {
+
+        }
+    })
+})
+
+// Form Templates
+
+app.get('/api/template/:template_id', (req, res) => {
+
+})
+
+// Clinics
+
+app.get('/api/clinic/:clinic_id', (res, req) => {
+
+})
+
+app.get('/api/clinic/:clinic_id/patients', (req, res) => {
+    let id = req.params.id
+    // Return list of patients
+})
+
+app.post('/api/clinic/:clinic_id/patients/:patient_id', (req, res) => {
+    // Add patient to client
 })
 
 app.listen(port, () => {
