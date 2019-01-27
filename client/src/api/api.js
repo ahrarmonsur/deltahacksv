9238eaf22e7da2587c0bc80be27667d9e7b3450f
deltalite <35876425+deltalite@users.noreply.github.com>
2019-01-27 00:53:16 -0500

    api methods exported from api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,19 @@
 import axios from 'axios'
 
-function submitPatientForm(patientID,patientInfo, ResAction){
-    axios.post(`/api/patients/${patientID}/form`, patientInfo).then(res =>
-    console.log("ResAction called"));
+function createPatient(patientBasicInfo){
+    axios.post('/api/patients', patientBasicInfo).then(res =>
+        console.log(`${patientBasicInfo['first_name']} added to db.`))
 }
-export {submitPatientForm}
+
+function submitPatientForm(patientID, patientData){
+    axios.post(`/api/patients/${patientID}/form`, patientData).then(res =>
+        console.log("ResAction called"));
+}
+
+
+function addPatientToClinic(clinicID, patientID){
+    axios.post(`/api/clinic/${clinicID}/patient/${patientID}`).then(res =>
+        console.log(`${patientID} added to ${clinicID}`))
+}
+
+export {createPatient, submitPatientForm, addPatientToClinic}
