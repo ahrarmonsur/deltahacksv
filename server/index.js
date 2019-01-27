c3489a292252eb82e7a3b532a094632336012bde
Caleb Mech <cmech1@gmail.com>
2019-01-26 20:07:49 -0500

    Prettier!!

--- server/index.js
+++ server/index.js
@@ -1,147 +1,165 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
-const ObjectId = require('mongodb').ObjectID
+const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
 
-const DB_URL = "mongodb://admin:dh5acdg@ds145868.mlab.com:45868/deltahacksv"
-const DB_NAME = "deltahacksv"
-const client = new MongoClient(DB_URL)
-let db
+const DB_URL = "mongodb://admin:dh5acdg@ds145868.mlab.com:45868/deltahacksv";
+const DB_NAME = "deltahacksv";
+const client = new MongoClient(DB_URL);
+let db;
 
-client.connect((err) => {
-    if (err) console.log(err)
-    console.log("Connected successfully to database")
+client.connect(err => {
+    if (err) console.log(err);
+    console.log("Connected successfully to database");
 
-    db = client.db(DB_NAME)
-})
+    db = client.db(DB_NAME);
+});
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello world")
-})
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
 
 // Patients
 
-app.get('/api/patients/:patient_id', (req, res) => {
-    let id = req.params.patient_id
+app.get("/api/patients/:patient_id", (req, res) => {
+    let id = req.params.patient_id;
 
-    db.collection('patients').findOne({"_id": ObjectId(id)}, (err, result) => {
-        if (err) console.log(err)
-        res.json(result)
-    })
-})
+    db.collection("patients").findOne({ _id: ObjectId(id) }, (err, result) => {
+        if (err) console.log(err);
+        res.json(result);
+    });
+});
 
-app.post('/api/patients', (req, res) => {
+app.post("/api/patients", (req, res) => {
     // Create patients
-    let patient = req.body
-    patient.dob = new Date(patient.dob)
+    let patient = req.body;
+    patient.dob = new Date(patient.dob);
 
-    db.collection('patients').insertOne(patient, (err, result) => {
-        if (err) console.log(err)
-        res.json(result.ops)
-    })
-})
+    db.collection("patients").insertOne(patient, (err, result) => {
+        if (err) console.log(err);
+        res.json(result.ops);
+    });
+});
 
-app.get('/api/form/:form_id', (req, res) => {
+app.get("/api/form/:form_id", (req, res) => {
     // Get form data
-    let id = req.params.form_id
+    let id = req.params.form_id;
 
-    db.collection('forms').findOne({"_id": ObjectId(id)}, (err, result) => {
-        if (err) console.log(err)
-        res.json(result)
-    })
-})
+    db.collection("forms").findOne({ _id: ObjectId(id) }, (err, result) => {
+        if (err) console.log(err);
+        res.json(result);
+    });
+});
 
-app.post('/api/patients/:patient_id/form', (req, res) => {
+app.post("/api/patients/:patient_id/form", (req, res) => {
     // Create/Update patients form
 
-    return_url = `http://localhost:3000/api/patients/${req.params.patient_id}`
+    return_url = `http://localhost:3000/api/patients/${req.params.patient_id}`;
 
-    payload = { ...req.body, created: new Date() }
+    payload = { ...req.body, created: new Date() };
 
-    db.collection('patients').findOne({"_id": ObjectId(req.params.patient_id)}, (err, patient) => {
-        console.log(patient.form)
-        if (patient.form) {
-            db.collection('forms').updateOne({"_id":ObjectId(patient.form)},
-            {
-                $set: payload
-            },
-        (err, result) => 
-            {
-                res.send(return_url)
-            })
-        } else {
-            db.collection('forms').insertOne(payload, (err, form_result) => {
-                db.collection('patients').updateOne({"_id": ObjectId(req.params.patient_id)}, {
-                    $set: {
-                        form: form_result.ops[0]._id
+    db.collection("patients").findOne(
+        { _id: ObjectId(req.params.patient_id) },
+        (err, patient) => {
+            console.log(patient.form);
+            if (patient.form) {
+                db.collection("forms").updateOne(
+                    { _id: ObjectId(patient.form) },
+                    {
+                        $set: payload
+                    },
+                    (err, result) => {
+                        res.send(return_url);
                     }
-                }, (err, patient_result) => {
-                    res.send(return_url)
-                })
-            })
+                );
+            } else {
+                db.collection("forms").insertOne(
+                    payload,
+                    (err, form_result) => {
+                        db.collection("patients").updateOne(
+                            { _id: ObjectId(req.params.patient_id) },
+                            {
+                                $set: {
+                                    form: form_result.ops[0]._id
+                                }
+                            },
+                            (err, patient_result) => {
+                                res.send(return_url);
+                            }
+                        );
+                    }
+                );
+            }
         }
-
-    })
-    
-})
+    );
+});
 
 // Form Templates
 
-app.get('/api/template/:template_id', (req, res) => {
-    let id = req.params.template_id
+app.get("/api/template/:template_id", (req, res) => {
+    let id = req.params.template_id;
 
-    db.collection('templates').findOne({"_id": id == "default" ? id : ObjectId(id)}, (err, result) => {
-        if (err) console.log(err)
-        res.json(result)
-    })
-})
+    db.collection("templates").findOne(
+        { _id: id == "default" ? id : ObjectId(id) },
+        (err, result) => {
+            if (err) console.log(err);
+            res.json(result);
+        }
+    );
+});
 
 // Clinics
 
-app.get('/api/clinic/:clinic_id', (req, res) => {
-    let id = req.params.clinic_id
+app.get("/api/clinic/:clinic_id", (req, res) => {
+    let id = req.params.clinic_id;
 
-    db.collection('clinics').findOne({"_id": ObjectId(id)}, (err, clinic) => {
-        clinic.patients = clinic.patients.map(patient => ObjectId(patient.oid))
-        
-        db.collection('patients').find({_id: {$in: clinic.patients}}).toArray((err, patients) => {
-            result = clinic
-            result.patients = patients
-            res.json(clinic)
-        })
-    })
-})
+    db.collection("clinics").findOne({ _id: ObjectId(id) }, (err, clinic) => {
+        clinic.patients = clinic.patients.map(patient => ObjectId(patient.oid));
 
-app.post('/api/clinic/:clinic_id/patient/:patient_id', (req, res) => {
+        db.collection("patients")
+            .find({ _id: { $in: clinic.patients } })
+            .toArray((err, patients) => {
+                result = clinic;
+                result.patients = patients;
+                res.json(clinic);
+            });
+    });
+});
+
+app.post("/api/clinic/:clinic_id/patient/:patient_id", (req, res) => {
     // Add patient to clinic
-    let clinic_id = req.params.clinic_id
-    let patient_id = req.params.patient_id
-    
-    db.collection('clinics').updateOne({"_id": ObjectId(clinic_id)}, {
-        $addToSet: {'patients': ObjectId(patient_id)}
-    }, (err, result) => {
-        console.log(result)
-        if (err) console.log(err)
-        else res.send("Success!")
-    })
-})
+    let clinic_id = req.params.clinic_id;
+    let patient_id = req.params.patient_id;
 
-app.post('/*', (req, res) => {
+    db.collection("clinics").updateOne(
+        { _id: ObjectId(clinic_id) },
+        {
+            $addToSet: { patients: ObjectId(patient_id) }
+        },
+        (err, result) => {
+            console.log(result);
+            if (err) console.log(err);
+            else res.send("Success!");
+        }
+    );
+});
+
+app.post("/*", (req, res) => {
     // Invalid post request
-    res.send("Invalid post request made.")
-})
+    res.send("Invalid post request made.");
+});
 
-app.get('/*', (req, res) => {
+app.get("/*", (req, res) => {
     // Invalid get request
-    res.send("Invalid get request made.")
-})
+    res.send("Invalid get request made.");
+});
 
 app.listen(port, () => {
-    console.log(`Server listening on ${port}`)
-})
+    console.log(`Server listening on ${port}`);
+});
